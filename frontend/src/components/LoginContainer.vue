345983f68395dd85fd16a1ffcd1f28ce84ed4ebb
Florian Geckeler <floge77@gmail.com>
2022-11-13 17:30:13 -0300

    extract some css

--- frontend/src/components/LoginContainer.vue
+++ frontend/src/components/LoginContainer.vue
@@ -1,6 +1,6 @@
 <script lang="ts">
-import { backendURL } from "../main"
-import { JwtResponse, LoginModel } from '../models/UserModels'
+import { backendURL } from "../main";
+import { JwtResponse, LoginModel } from "../models/UserModels";
 
 export default {
   data() {
@@ -16,22 +16,19 @@ export default {
       }
 
       try {
-        let user: LoginModel = new LoginModel(
-          this.username,
-          this.password
-        )
+        let user: LoginModel = new LoginModel(this.username, this.password);
         const response = await fetch(`${backendURL}/auth/login`, {
           method: "POST",
           body: JSON.stringify(user),
           headers: { "Content-Type": "application/json; charset=UTF-8" },
-        })
+        });
 
-        if(!response.ok) {
-          throw new Error(`Error! status: ${response.status}`)
+        if (!response.ok) {
+          throw new Error(`Error! status: ${response.status}`);
         }
 
-        const result = (await response.json() as JwtResponse)
-        console.log(`successfully logged in. JWT: ${result.token}`)
+        const result = (await response.json()) as JwtResponse;
+        console.log(`successfully logged in. JWT: ${result.token}`);
       } catch (e) {
         console.log(`failed login for user: ${this.username}: ${e}`);
       }
@@ -41,39 +38,11 @@ export default {
 </script>
 
 <template>
-  <div class="register-parent">
-    <form @submit.prevent="login" class="register-child">
+  <div >
+    <form @submit.prevent="login" class="register-login-child">
       <input type="username" placeholder="Username..." v-model="username" />
       <input type="password" placeholder="password..." v-model="password" />
       <button type="submit">Login</button>
     </form>
   </div>
 </template>
-
-<style scoped>
-.register-parent {
-  max-width: 100%;
-  margin-top: 1rem;
-}
-.register-child {
-  max-width: 300px;
-  display: grid;
-  grid-template-columns: auto;
-  margin: 0 auto 0 auto;
-}
-input {
-  width: auto;
-  height: auto;
-  margin-bottom: 10px;
-  align-self: center;
-}
-input:focus {
-  outline: none !important;
-  border: 2px solid #f8874f;
-}
-button {
-  margin-top: 10px;
-
-  cursor: pointer;
-}
-</style>
--- frontend/src/components/RegisterContainer.vue
+++ frontend/src/components/RegisterContainer.vue
@@ -1,6 +1,6 @@
 <script lang="ts">
 import { MessageResponse, RegisterModel } from "@/models/UserModels";
-import { backendURL } from "../main"
+import { backendURL } from "../main";
 
 export default {
   data() {
@@ -16,25 +16,25 @@ export default {
         `New user will be registered: username: ${this.username}, email: ${this.email}, password: ${this.password}`
       );
       if (!(this.username && this.password && this.email)) {
-        return
+        return;
       }
 
       try {
         let user: RegisterModel = new RegisterModel(
-            this.username,
-            this.email,
-            this.password
-        )
+          this.username,
+          this.email,
+          this.password
+        );
         const response = await fetch(`${backendURL}/auth/register`, {
           method: "POST",
           body: JSON.stringify(user),
           headers: { "Content-Type": "application/json; charset=UTF-8" },
-        })
-        if(!response.ok) {
-          throw new Error(`Error! status: ${response.status}`)
+        });
+        if (!response.ok) {
+          throw new Error(`Error! status: ${response.status}`);
         }
-        const result = (await response.json() as MessageResponse)
-        console.log(`user created. response: ${result.message}`)
+        const result = (await response.json()) as MessageResponse;
+        console.log(`user created. response: ${result.message}`);
       } catch (e) {
         console.log(`failed registration for user: ${this.username}: ${e}`);
       }
@@ -44,8 +44,8 @@ export default {
 </script>
 
 <template>
-  <div class="register-parend">
-    <form @submit.prevent="registerUser" class="register-child">
+  <div>
+    <form @submit.prevent="registerUser" class="register-login-child">
       <input type="username" placeholder="Username..." v-model="username" />
       <input type="email" placeholder="Email address..." v-model="email" />
       <input type="password" placeholder="password..." v-model="password" />
@@ -53,31 +53,3 @@ export default {
     </form>
   </div>
 </template>
-
-<style scoped>
-.register-parent {
-  max-width: 100%;
-  margin-top: 80px;
-}
-.register-child {
-  max-width: 300px;
-  display: grid;
-  grid-template-columns: auto;
-  margin: 0 auto 0 auto;
-}
-input {
-  width: auto;
-  height: auto;
-  margin-bottom: 10px;
-  align-self: center;
-}
-input:focus {
-  outline: none !important;
-  border: 2px solid #f8874f;
-}
-button {
-  margin-top: 10px;
-
-  cursor: pointer;
-}
-</style>
